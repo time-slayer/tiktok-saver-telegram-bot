e01b3a6ac4914cf4518815ffbaeba2a84723d498
TimeSlayer <nikitajudo313@gmail.com>
2025-09-19 22:21:55 +0300

    feat: add inline query support with video info extraction

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import os
 import logging
+from typing import Dict
+from yt_dlp import YoutubeDL
 from dotenv import load_dotenv
-from telegram import Update
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update, InlineQueryResultVideo
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes, InlineQueryHandler
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO)
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -32,15 +36,50 @@ async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
-def main():
+async def inline_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.inline_query.query
+    if not query:
+        return
+
+    video_info = extract_video_info(query)
+    result = InlineQueryResultVideo(
+        id='1',
+        video_url=video_info.get('url'),
+        mime_type='video/mp4',
+        thumbnail_url=video_info.get('thumbnail'),
+        title=video_info.get('title'),
+    )
+    await update.inline_query.answer([result])
+
+
+def extract_video_info(url: str) -> Dict[str, str] | None:
+    ydl_opts = {
+        'quiet': True,
+        'no_warnings': True,
+    }
+    try:
+        with YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=False)
+            return {
+                'title': info.get('title'),
+                'url': info.get('url'),
+                'thumbnail': info.get('thumbnail'),
+            }
+    except Exception as e:
+        logging.error(e)
+        return None
+
+
+def main() -> None:
     load_dotenv()
-    bot_token = os.getenv("TEST_BOT_TOKEN")
+    bot_token = os.getenv("BOT_TOKEN")
     application = ApplicationBuilder().token(bot_token).build()
 
     application.add_handler(CommandHandler('start', start))
     application.add_handler(CommandHandler('help', help_command))
-
     application.add_handler(MessageHandler(filters.COMMAND, unknown))
+
+    application.add_handler(InlineQueryHandler(inline_video))
 
     application.run_polling()
 
